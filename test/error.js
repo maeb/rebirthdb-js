4ab27f760f5e8edd595c00f708a0405746a25ffd
Marius André Elsfjordstrand Beck <marius.beck@nb.no>
2018-05-30 11:11:31 +0200

    test: refactor error.js

--- test/error.js
+++ test/error.js
@@ -1,52 +1,59 @@
-var config = require(__dirname+'/config.js');
-var r = require(__dirname+'/../lib')(config);
-var util = require(__dirname+'/util/common.js');
-var assert = require('assert');
+const path = require('path')
+const config = require('./config.js')
+const rethinkdbdash = require(path.join(__dirname, '/../lib'))
+const assert = require('assert')
 
-var uuid = util.uuid;
-var It = util.It;
+const {before, after, describe, it} = require('mocha')
 
-var uuid = util.uuid;
-var dbName, tableName, result;
+describe('errors', () => {
+  let r
 
-It("ReqlResourceError", function* (done) {
-  try {
-    result = yield r.expr([1,2,3,4]).run({arrayLimit: 2});
-  }
-  catch(e) {
-    assert.equal(e.name, 'ReqlResourceError');
-    done()
-  }
+  before(async () => {
+    r = await rethinkdbdash(config)
+  })
+
+  after(async () => {
+    await r.getPoolMaster().drain()
+  })
+
+  it('ReqlResourceError', async function () {
+    try {
+      await r.expr([1, 2, 3, 4]).run({arrayLimit: 2})
+      assert.fail('should throw')
+    } catch (e) {
+      assert.equal(e.name, 'ReqlResourceError')
+    }
+  })
+
+  it('ReqlLogicError', async function () {
+    try {
+      await r.expr(1).add('foo').run()
+      assert.fail('should throw')
+    } catch (e) {
+      assert.equal(e.name, 'ReqlLogicError')
+    }
+  })
+
+  it('ReqlOpFailedError', async function () {
+    try {
+      await r.db('DatabaseThatDoesNotExist').tableList().run()
+      assert.fail('should throw')
+    } catch (e) {
+      assert.equal(e.name, 'ReqlOpFailedError')
+    }
+  })
+
+  it('ReqlUserError', async function () {
+    try {
+      await r.branch(r.error('a'), 1, 2).run()
+      assert.fail('should throw')
+    } catch (e) {
+      assert.equal(e.name, 'ReqlUserError')
+    }
+  })
+
+  describe('Missing tests', function () {
+    it('ReqlInternalError no easy way to trigger', function () {})
+    it('ReqlOpIndeterminateError no easy way to trigger', function () {})
+  })
 })
-It("ReqlLogicError", function* (done) {
-  try {
-    result = yield r.expr(1).add("foo").run();
-  }
-  catch(e) {
-    assert.equal(e.name, 'ReqlLogicError');
-    done()
-  }
-})
-
-It("ReqlOpFailedError", function* (done) {
-  try {
-    result = yield r.db('DatabaseThatDoesNotExist').tableList().run();
-  }
-  catch(e) {
-    assert.equal(e.name, 'ReqlOpFailedError');
-    done()
-  }
-})
-
-It("ReqlUserError", function* (done) {
-  try {
-    result = yield r.branch(r.error('a'), 1, 2).run()
-  }
-  catch(e) {
-    assert.equal(e.name, 'ReqlUserError');
-    done()
-  }
-})
-
-// Missing tests for ReqlInternalError and ReqlOpIndeterminateError
-// as there are no easy way to trigger those
