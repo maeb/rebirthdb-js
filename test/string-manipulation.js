97fbcf58c4511e95d985fa2db855bd49cbf7a8a2
Marius André Elsfjordstrand Beck <marius.beck@nb.no>
2018-06-04 00:34:40 +0200

    test: refactor string-manipulation.js

--- test/string-manipulation.js
+++ test/string-manipulation.js
@@ -1,86 +1,56 @@
-var config = require(__dirname+'/config.js');
-var r = require(__dirname+'/../lib')(config);
-var util = require(__dirname+'/util/common.js');
-var assert = require('assert');
+const path = require('path')
+const config = require('./config.js')
+const rethinkdbdash = require(path.join(__dirname, '/../lib'))
+const assert = require('assert')
+const {before, after, describe, it} = require('mocha')
 
-var uuid = util.uuid;
-var It = util.It;
+describe('string manipulation', () => {
+  let r
 
-var uuid = util.uuid;
-var dbName, tableName, result;
+  before(async () => {
+    r = await rethinkdbdash(config)
+  })
 
+  after(async () => {
+    await r.getPool().drain()
+  })
 
-It('`match` should work', function* (done) {
-  try {
-    result = yield r.expr("hello").match("hello").run()
-    assert.deepEqual(result, {"end":5,"groups":[],"start":0,"str":"hello"});
-    done();
-  }
-  catch(e) {
-    done(e);
-  }
-})
-It('`match` should throw if no arguement has been passed', function* (done) {
-  try {
-    result = yield r.expr("foo").match().run();
-  }
-  catch(e) {
-    if (e.message === "`match` takes 1 argument, 0 provided after:\nr.expr(\"foo\")") {
-      done();
+  it('`match` should work', async function () {
+    const result = await r.expr('hello').match('hello').run()
+    assert.deepEqual(result, {'end': 5, 'groups': [], 'start': 0, 'str': 'hello'})
+  })
+
+  it('`match` should throw if no arguement has been passed', async function () {
+    try {
+      await r.expr('foo').match().run()
+      assert.fail('should throw')
+    } catch (e) {
+      assert.equal(e.message, '`match` takes 1 argument, 0 provided after:\nr.expr("foo")')
     }
-    else {
-      done(e);
-    }
-  }
-})
-It('`upcase` should work', function* (done) {
-  try {
-    result = yield r.expr("helLo").upcase().run();
-    assert.equal(result, "HELLO");
-    done();
-  }
-  catch(e) {
-    done(e);
-  }
-})
-It('`downcase` should work', function* (done) {
-  try {
-    result = yield r.expr("HElLo").downcase().run();
-    assert.equal(result, "hello");
-    done();
-  }
-  catch(e) {
-    done(e);
-  }
-})
-It('`split` should work', function* (done) {
-  try {
-    result = yield r.expr("foo  bar bax").split().run();
-    assert.deepEqual(result, ["foo",  "bar", "bax"]);
-    done();
-  }
-  catch(e) {
-    done(e);
-  }
-})
-It('`split(separator)` should work', function* (done) {
-   try {
-    result = yield r.expr("12,37,,22,").split(",").run();
-    assert.deepEqual(result, ["12", "37", "", "22", ""]);
-    done();
-  }
-  catch(e) {
-    done(e);
-  }
-})
-It('`split(separtor, max)` should work', function* (done) {
-  try {
-    result = yield r.expr("foo  bar bax").split(null, 1).run();
-    assert.deepEqual(result, ["foo", "bar bax"]);
-    done();
-  }
-  catch(e) {
-    done(e);
-  }
-})
+  })
 
+  it('`upcase` should work', async function () {
+    const result = await r.expr('helLo').upcase().run()
+    assert.equal(result, 'HELLO')
+  })
+
+  it('`downcase` should work', async function () {
+    const result = await r.expr('HElLo').downcase().run()
+    assert.equal(result, 'hello')
+  })
+
+  it('`split` should work', async function () {
+    const result = await r.expr('foo  bar bax').split().run()
+    assert.deepEqual(result, ['foo', 'bar', 'bax'])
+  })
+
+  it('`split(separator)` should work', async function () {
+    const result = await r.expr('12,37,,22,').split(',').run()
+    assert.deepEqual(result, ['12', '37', '', '22', ''])
+  })
+
+  it('`split(separtor, max)` should work', async function () {
+    const result = await r.expr('foo  bar bax').split(null, 1).run()
+    assert.deepEqual(result, ['foo', 'bar bax'])
+  })
+})
